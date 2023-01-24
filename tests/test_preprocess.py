999777e25e3393aa123787e924f215bb8b5324b2
Monica Shapiro <monshapiro@gmail.com>
2023-01-24 10:45:51 -0500

    Updated test script

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -6,5 +6,7 @@ import numpy as np
 sys.path.append(os.path.abspath("../"))
 import psanalysis.analyze_grids as ag
 
-test_dir = ag.PlanarStudy(os.getcwd(), np.arange(0, 80, 2))
+cwd = os.getcwd()
+tpts = np.arange(0, 80, 2)
+test_dir = ag.PlanarStudy(os.path.join(cwd, "SampleImages"), tpts)
 test_dir.preprocess_scans(debug=True)
