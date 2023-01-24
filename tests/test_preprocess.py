831fe4066e4ad6093bed52b5687f6e1979363cea
Monica Shapiro <monshapiro@gmail.com>
2023-01-24 11:33:27 -0500

    Added test of gridify method

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -9,4 +9,5 @@ import psanalysis.analyze_grids as ag
 wd = os.path.dirname(os.path.realpath(__file__))
 tpts = np.arange(0, 80, 2)
 test_dir = ag.PlanarStudy(os.path.join(wd, "SampleImages"), tpts)
-test_dir.preprocess_scans(debug=True)
+test_dir.preprocess_scans(debug=False)
+(a, grid_aTc, grid_aIn, grid_pTc, grid_pIn) = test_dir.gridify(16, 8)
